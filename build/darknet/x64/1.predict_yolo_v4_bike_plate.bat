9956c9e8e2eac9fe16a22e8cf37f50727f230d58
vohungvi <vohungvi27@gmail.com>
2021-06-13 12:54:21 +0700

    + add image

--- build/darknet/x64/1.predict_yolo_v4_bike_plate.bat
+++ build/darknet/x64/1.predict_yolo_v4_bike_plate.bat
@@ -1,2 +1,2 @@
-darknet.exe detector test data/bike_plate/bike_plate.data cfg/yolov4-tiny-bike-plate.cfg backup/yolov4-tiny-bike-plate.weights image_to_test\bike_plate\0112_01593_b.jpg
+darknet.exe detector test data/bike_plate/bike_plate.data data/bike_plate/yolov4-tiny-bike_plate.cfg backup/yolov4-tiny-bike-plate.weights image_to_test\bike_plate\2kk.24_2021-03-07.16-47-37.jpg
 pause
